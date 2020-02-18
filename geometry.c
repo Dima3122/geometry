271a6c09f7a4bcecb8e7a7ca3cb76da3403403b0
Dima3122 <dimasavostin89@gmail.com>
2020-02-18 19:15:28 +0700

    Added data entry to the application

---
+++ geometry.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	int x1,y1,r;
+    printf("Круг ");
+    scanf("%d", &x1);
+    scanf("%d",&y1);
+    scanf("%d", &r);
+    int xt1, xt2, yt1, yt2, xt3, yt3;
+    printf("Треугольник");
+    scanf("%d", &xt1);
+    scanf("%d", &yt1);
+    scanf("%d", &xt2);
+    scanf("%d", &yt2);
+    scanf("%d",&xt3);
+    scanf("%d", &yt3);
+
+	return 0;
+}
