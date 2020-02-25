08b77beff96317065aa3a66038c3ccaaa2640cb2
Dima3122 <dimasavostin89@gmail.com>
2020-02-25 09:03:40 +0700

    Added check for extraneous characters

--- geometry.c
+++ geometry.c
@@ -2,6 +2,7 @@
 #include <math.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 //Вычесление сторон треугольника 
 float Sidelength(int a, int b, int c, int d)
 {   
@@ -30,10 +31,19 @@ int main()
 {
 
     int x1,y1,r;
+    printf("     x y r \n");
     printf("Круг ");
     scanf("%d %d %d", &x1, &y1, &r);
     int xt1, xt2, yt1, yt2, xt3, yt3;
     //Проверка радиуса окружности 
+    int a=isalpha(x1);
+    int b=isalpha(y1);
+    int c=isalpha(r);
+
+    if (a!=0 ||b!=0 || c!=0)
+        {
+            exit(1);
+        }    
         while(r<=0)
     {
         printf("Введите другое число больше 0  ");
@@ -46,8 +56,21 @@ int main()
     float Plosk = M_PI*r*r;
     printf("Площадь круга равна: %f\n", Plosk);
    
-    printf("Треугольник");
+    printf("            x1 y1 x2 y2 x3 y3 \n");
+    printf("Треугольник ");
     scanf("%d %d %d %d %d %d", &xt1, &yt1, &xt2, &yt2, &xt3, &yt3);
+
+    int q=isalpha(xt1);
+    int w=isalpha(yt1); 
+    int e=isalpha(xt2);
+    int s=isalpha(yt2);
+    int t=isalpha(xt3);
+    int y=isalpha(yt3);
+
+    while(q!=0 ||w!=0 ||e!=0||s!=0 ||t!=0||y!=0)
+    {
+        exit(1);
+    }
        
     proverka(&xt1, &yt1, &xt2, &yt2, &xt3, &yt3);
     //вычисление 3х сторон треугольника
@@ -62,5 +85,5 @@ int main()
     float Plos_t=polyper*sqrt(sqrt(polyper-ab))+(sqrt(polyper-ac))+(sqrt(polyper-bc));
     printf("Площадь треугольника равна: %f\n",Plos_t);
 
-	return 0;
+    return 0;
 }
