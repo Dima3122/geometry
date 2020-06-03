41014f92b281ca3a83eea1e3cfdda1d7c5b40a42
Dima3122 <dimasavostin89@gmail.com>
2020-06-03 16:40:35 +0700

    make somechanges

--- src/geometry.h
+++ src/geometry.h
@@ -6,7 +6,7 @@
 
 float Sidelength(int a, int b, int c, int d);
 void proverka(int* x1, int* y1, int* x2, int* y2, int* x3, int* y3);
-void checkcircle(int x1, int y1, int *r);
+void checkcircle(int x1, int y1, int* r);
 float perimeter_circle(int r);
 float area_circle(int r);
 float per_treyg(int xt1, int xt2, int yt1, int yt2, int xt3, int yt3);
